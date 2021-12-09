d8b05befbc6e203931ad82b2cd98da039f1c235f
bohdan.fedorchenko <bogdan.fedorchenko@tatemgames.com>
2021-12-09 18:09:45 +0200

    Used sha512 before running argon2

--- security-server/Hashing/ArgonPasswordHasher.cs
+++ security-server/Hashing/ArgonPasswordHasher.cs
@@ -1,10 +1,13 @@
 namespace Security.Server.Hashing
 {
+    using System.Security.Cryptography;
+    using System.Text;
     using Microsoft.AspNetCore.Identity;
 
     public class ArgonPasswordHasher<TUser> : IPasswordHasher<TUser> where TUser : class
     {
         private readonly Argon2 argon = new();
+        private readonly SHA512 sha512 = new SHA512Managed();
 
         public string HashPassword(TUser user, string password)
         {
@@ -14,6 +17,7 @@ namespace Security.Server.Hashing
         public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword,
             string providedPassword)
         {
+            providedPassword = TruncatePassword(providedPassword);
             var isValid = argon.Verify(hashedPassword, providedPassword);
             if (!isValid)
             {
@@ -25,6 +29,13 @@ namespace Security.Server.Hashing
                 return PasswordVerificationResult.SuccessRehashNeeded;
             }
             return PasswordVerificationResult.Success;
+        }
+
+        private string TruncatePassword(string providedPassword)
+        {
+            var data = Encoding.UTF8.GetBytes(providedPassword);
+            var hash = sha512.ComputeHash(data);
+            return Encoding.UTF8.GetString(hash);
         }
     }
 }
